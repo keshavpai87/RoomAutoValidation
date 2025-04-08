942cadd23062ffd60c6713b423b65b7f172a5441
Keshav Pai <keshu.pai@gmail.com>
2025-04-08 14:47:11 +0530

    Working code for Auto Migration

--- app/src/main/java/com/example/roomautomigration/db/StudentDatabase.kt
+++ app/src/main/java/com/example/roomautomigration/db/StudentDatabase.kt
@@ -8,8 +8,8 @@ import androidx.room.RoomDatabase
 
 @Database(
     entities = [Student::class],
-    version = 4,
-    autoMigrations = [AutoMigration(from = 3, to = 4)]
+    version = 3,
+    autoMigrations = [AutoMigration(from = 2, to = 3)]
 )
 abstract class StudentDatabase : RoomDatabase() {
 
